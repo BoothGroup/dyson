b3fe5f2290bd05f72cf678dd3289980e67eeafc6
Ollie Backhouse <olbackhouse@gmail.com>
2023-02-14 10:54:17 +0000

    Logging fixes

--- dyson/util.py
+++ dyson/util.py
@@ -16,7 +16,7 @@ def format_value(val, prec=8):
         return "%.*f" % (prec, val)
     else:
         op = "+" if val.imag >= 0 else "-"
-        return "%.*f %s %.*f" % (prec, val.real, op, prec, np.abs(val.imag))
+        return "%.*f%s%.*fj" % (prec, val.real, op, prec, np.abs(val.imag))
 
 
 def print_eigenvalues(eigvals, nroots=5, abs_sort=True, header=True):
@@ -24,23 +24,23 @@ def print_eigenvalues(eigvals, nroots=5, abs_sort=True, header=True):
     Return a string summarising some eigenvalues.
     """
 
-    lines = ["-" * 25]
+    lines = ["-" * 30]
     if header:
-        lines += ["{:^25s}".format("Eigenvalue summary")]
-        lines += ["-" * 25]
+        lines += ["{:^30s}".format("Eigenvalue summary")]
+        lines += ["-" * 30]
     lines += [
-        "%4s %20s" % ("Root", "Value"),
-        "%4s %20s" % ("-" * 4, "-" * 20),
+        "%4s %25s" % ("Root", "Value"),
+        "%4s %25s" % ("-" * 4, "-" * 25),
     ]
 
     inds = np.argsort(np.abs(eigvals.real)) if abs_sort else np.argsort(eigvals.real)
     for i in inds[: min(nroots, len(eigvals))]:
-        lines.append("%4d %20s" % (i, format_value(eigvals[i])))
+        lines.append("%4d %25s" % (i, format_value(eigvals[i])))
 
     if nroots < len(eigvals):
         lines.append(" ...")
 
-    lines.append("-" * 25)
+    lines.append("-" * 30)
 
     return "\n".join(lines)
 
@@ -53,44 +53,49 @@ def print_dyson_orbitals(
     into the physical space, resulting in Dyson orbitals.
     """
 
-    lines = ["-" * 93]
+    lines = ["-" * 98]
     if header:
-        lines += ["{:^93s}".format("Dyson orbital summary")]
-        lines += ["-" * 93]
+        lines += ["{:^98s}".format("Dyson orbital summary")]
+        lines += ["-" * 98]
     lines += [
-        "{:>4s} {:^20s} {:^33s} {:^33s}".format("", "", "Weight", ""),
-        "{:>4s} {:^20s} {:^33s} {:^33s}".format(
+        "{:>4s} {:^25s} {:^33s} {:^33s}".format("", "", "Weight", ""),
+        "{:>4s} {:^25s} {:^33s} {:^33s}".format(
             "Orb",
             "Energy",
             "-" * 33,
             "Dominant physical contributions",
         ),
-        "{:>4s} {:^20s} {:>16s} {:>16s} {:>16s}".format("", "", "Physical", "Auxiliary", ""),
-        "{:>4s} {:^20s} {:>16s} {:>16s} {:>16s}".format(
+        "{:>4s} {:^25s} {:>16s} {:>16s} {:>16s}".format("", "", "Physical", "Auxiliary", ""),
+        "{:>4s} {:^25s} {:>16s} {:>16s} {:>16s}".format(
             "-" * 4,
-            "-" * 20,
+            "-" * 25,
             "-" * 16,
             "-" * 16,
             "-" * 33,
         ),
     ]
 
-    mask = np.linalg.norm(eigvecs[:nphys], axis=0) ** 2 > phys_threshold
+    if isinstance(eigvecs, tuple):
+        eigvecs_l, eigvecs_r = eigvecs
+    else:
+        eigvecs_l = eigvecs_r = eigvecs
+
+    mask = np.sum(eigvecs_l * eigvecs_r.conj(), axis=0) > phys_threshold
     inds = np.arange(eigvals.size)[mask]
     inds = inds[
         np.argsort(np.abs(eigvals[inds].real)) if abs_sort else np.argsort(eigvals[inds].real)
     ]
     for i in inds[: min(nroots, len(eigvals))]:
-        v = eigvecs[:, i]
-        phys = np.linalg.norm(np.abs(v[:nphys])) ** 2
-        aux = np.linalg.norm(np.abs(v[nphys:])) ** 2
+        v = eigvecs_l[:, i] * eigvecs_r[:, i].conj()
+        phys = np.sum(v[:nphys])
+        aux = np.sum(v[nphys:])
         chars = []
-        for j in np.argsort(np.abs(v[:nphys]) ** 2):
-            if eigvecs[j, i] ** 2 > 0.2:
-                chars.append("%d (%.2f)" % (j, np.abs(eigvecs[j, i]) ** 2))
+        for j in np.argsort(v[:nphys]):
+            if v[j] ** 2 > 0.2:
+                chars.append("%d (%.2f)" % (j, v[j]))
         chars = ", ".join(chars)
         lines.append(
-            "%4d %20s %16.3g %16.3g %33s"
+            "%4d %25s %16.3g %16.3g %33s"
             % (
                 i,
                 format_value(eigvals[i]),
@@ -103,7 +108,7 @@ def print_dyson_orbitals(
     if nroots < len(inds):
         lines.append(" ...")
 
-    lines += ["-" * 93]
+    lines += ["-" * 98]
 
     return "\n".join(lines)
 
