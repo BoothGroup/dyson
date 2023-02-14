11fc5e27da53b7b6ff897536bee534d2b21cf366
Ollie Backhouse <olbackhouse@gmail.com>
2023-02-14 14:49:01 +0000

    Stop isort from causing circular import

--- dyson/solvers/__init__.py
+++ dyson/solvers/__init__.py
@@ -1,6 +1,6 @@
 from dyson.solvers.solver import BaseSolver
-from dyson.solvers.davidson import Davidson
 from dyson.solvers.exact import Exact
-from dyson.solvers.mblgf import MBLGF
-from dyson.solvers.mblse import MBLSE
+from dyson.solvers.davidson import Davidson
 from dyson.solvers.self_consistent import DiagonalSelfConsistent, SelfConsistent
+from dyson.solvers.mblse import MBLSE
+from dyson.solvers.mblgf import MBLGF
