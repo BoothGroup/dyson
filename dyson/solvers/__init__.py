6da7cf0b09f22aea1ee90880566e66a57888f4f0
Ollie Backhouse <olbackhouse@gmail.com>
2023-04-17 14:34:41 +0100

    Remove accidental commit

--- dyson/solvers/__init__.py
+++ dyson/solvers/__init__.py
@@ -5,7 +5,6 @@ from dyson.solvers.downfolded import DiagonalDownfolded, Downfolded
 from dyson.solvers.mblse import MBLSE, MixedMBLSE
 from dyson.solvers.mblgf import MBLGF, MixedMBLGF
 from dyson.solvers.kpmgf import KPMGF
-from dyson.solvers.cpgf import CPGF
 from dyson.solvers.chempot import AufbauPrinciple, AuxiliaryShift
 from dyson.solvers.density import DensityRelaxation
 from dyson.solvers.self_consistent import SelfConsistent
