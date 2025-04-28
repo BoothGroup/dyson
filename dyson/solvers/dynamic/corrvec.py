58155327dabc67813cb9646cd123d85f7cf92bbf
Oliver Backhouse <olbackhouse@gmail.com>
2025-04-28 13:39:36 +0100

    Dynamic solver refactor

---
+++ dyson/solvers/dynamic/corrvec.py
@@ -0,0 +1,193 @@
+"""Correction vector Green's function solver."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from scipy.sparse.linalg import LinearOperator, gcrotmk
+
+from dyson import numpy as np
+from dyson.solvers.solver import DynamicSolver
+
+if TYPE_CHECKING:
+    from typing import Any, Callable
+
+    from dyson.typing import Array
+
+# TODO: (m,k) for GCROTMK, more solvers, DIIS
+
+
+class CorrectionVector(DynamicSolver):
+    """Correction vector Green's function solver.
+
+    Args:
+        matvec: The matrix-vector operation for the self-energy supermatrix.
+        diagonal: The diagonal of the self-energy supermatrix.
+        nphys: The number of physical degrees of freedom.
+        grid: Real frequency grid upon which to evaluate the Green's function.
+    """
+
+    def __init__(
+        self,
+        matvec: Callable[[Array], Array],
+        diagonal: Array,
+        nphys: int,
+        grid: Array,
+        get_state_bra: Callable[[int], Array] | None = None,
+        get_state_ket: Callable[[int], Array] | None = None,
+        eta: float = 1e-2,
+        trace: bool = False,
+        include_real: bool = True,
+    ):
+        r"""Initialise the solver.
+
+        Args:
+            matvec: The matrix-vector operation for the self-energy supermatrix.
+            diagonal: The diagonal of the self-energy supermatrix.
+            nphys: The number of physical degrees of freedom.
+            grid: Real frequency grid upon which to evaluate the Green's function.
+            get_state_bra: Function to get the bra vector corresponding to a fermion operator acting
+                on the ground state. If `None`, the state vector is :math:`v_{i} = \delta_{ij}` for
+                orbital :math:`j`.
+            get_state_ket: Function to get the ket vector corresponding to a fermion operator acting
+                on the ground state. If `None`, the :arg:`get_state_bra` function is used.
+            eta: The broadening parameter.
+            trace: Whether to return only the trace.
+            include_real: Whether to include the real part of the Green's function.
+        """
+        self._matvec = matvec
+        self._diagonal = diagonal
+        self._nphys = nphys
+        self._grid = grid
+        self._get_state_bra = get_state_bra
+        self._get_state_ket = get_state_ket
+        self.eta = eta
+        self.trace = trace
+        self.include_real = include_real
+
+    def matvec_dynamic(self, vector: Array, grid: Array) -> Array:
+        r"""Perform the matrix-vector operation for the dynamic self-energy supermatrix.
+
+        .. math::
+            \mathbf{x}_\omega = \left(\omega - \mathbf{H} - i\eta\right) \mathbf{r}
+
+        Args:
+            vector: The vector to operate on.
+            grid: The real frequency grid.
+
+        Returns:
+            The result of the matrix-vector operation.
+        """
+        result = (grid[:, None] - 1.0j * self.eta) * vector[None]
+        result -= self.matvec(vector.real)[None]
+        if np.any(np.abs(vector.imag) > 1e-14):
+            result -= self.matvec(vector.imag)[None] * 1.0j
+        return result
+
+    def matdiv_dynamic(self, vector: Array, grid: Array) -> Array:
+        r"""Approximately perform a matrix-vector division for the dynamic self-energy supermatrix.
+
+        .. math::
+            \mathbf{x}_\omega = \frac{\mathbf{r}}{\omega - \mathbf{H} - i\eta}
+
+        Args:
+            vector: The vector to operate on.
+            grid: The real frequency grid.
+
+        Returns:
+            The result of the matrix-vector division.
+
+        Notes:
+            The inversion is approximated using the diagonal of the matrix.
+        """
+        result = vector[None] / (grid[:, None] - self.diagonal[None] - 1.0j * self.eta)
+        result[np.isinf(result)] = np.nan  # or 0?
+        return result
+
+    def get_state_bra(self, orbital: int) -> Array:
+        """Get the bra vector corresponding to a fermion operator acting on the ground state.
+
+        Args:
+            orbital: Orbital index.
+
+        Returns:
+            Bra vector.
+        """
+        if self._get_state_bra is None:
+            return np.eye(self.nphys, 1, k=orbital).ravel()
+        return self._get_state_bra(orbital)
+
+    def get_state_ket(self, orbital: int) -> Array:
+        """Get the ket vector corresponding to a fermion operator acting on the ground state.
+
+        Args:
+            orbital: Orbital index.
+
+        Returns:
+            Ket vector.
+        """
+        if self._get_state_ket is None:
+            return self.get_state_bra(orbital)
+        return self._get_state_ket(orbital)
+
+    def kernel(self) -> Array:
+        """Run the solver.
+
+        Returns:
+            The Green's function on the real frequency grid.
+        """
+        # Precompute bra vectors  # TODO: Optional
+        bras = list(map(self.get_state_bra, range(self.nphys)))
+
+        # Loop over ket vectors
+        shape = (self.grid.size,) if self.trace else (self.grid.size, self.nphys, self.nphys)
+        greens_function = np.zeros(shape, dtype=complex)
+        for i in range(self.nphys):
+            ket = self.get_state_ket(i)
+
+            # Loop over frequencies
+            x: Array | None = None
+            for w in range(self.grid.size):
+                shape = (self.diagonal.size, self.diagonal.size)
+                matvec = LinearOperator(shape, lambda ω: self.matvec_dynamic(ket, ω), dtype=complex)
+                matdiv = LinearOperator(shape, lambda ω: self.matdiv_dynamic(ket, ω), dtype=complex)
+                if x is None:
+                    x = matdiv @ ket
+                x, info = gcrotmk(
+                    matvec,
+                    ket,
+                    x0=x,
+                    M=matdiv,
+                    atol=0.0,
+                    rtol=self.conv_tol,
+                )
+
+                if info != 0:
+                    greens_function[w] = np.nan
+                elif not self.trace:
+                    for j in range(self.nphys):
+                        greens_function[w, i, j] = bras[j] @ x
+                else:
+                    greens_function[w] += bras[i] @ x
+
+        return greens_function if self.include_real else greens_function.imag
+
+    @property
+    def matvec(self) -> Callable[[Array], Array]:
+        """Get the matrix-vector operation for the self-energy supermatrix."""
+        return self._matvec
+
+    @property
+    def diagonal(self) -> Array:
+        """Get the diagonal of the self-energy supermatrix."""
+        return self._diagonal
+
+    @property
+    def grid(self) -> Array:
+        """Get the real frequency grid."""
+        return self._grid
+
+    @property
+    def nphys(self) -> int:
+        """Get the number of physical degrees of freedom."""
+        return self.moments.shape[-1]
---
+++ dyson/solvers/dynamic/cpgf.py
@@ -0,0 +1,123 @@
+"""Chebyshev polynomial Green's function solver."""
+
+from __future__ import annotations
+
+import functools
+from typing import TYPE_CHECKING
+
+from dyson import numpy as np, util
+from dyson.solvers.solver import DynamicSolver
+
+if TYPE_CHECKING:
+    from typing import Any
+
+    from dyson.typing import Array
+
+einsum = functools.partial(np.einsum, optimize=True)  # TODO: Move
+
+
+def _infer_max_cycle(moments: Array) -> int:
+    """Infer the maximum number of cycles from the moments."""
+    return moments.shape[0] - 1
+
+
+class CPGF(DynamicSolver):
+    """Chebyshev polynomial Green's function solver [1]_.
+
+    Args:
+        moments: Chebyshev moments of the Green's function.
+        grid: Real frequency grid upon which to evaluate the Green's function.
+        scaling: Scaling factors to ensure the energy scale of the Lehmann representation is in
+            `[-1, 1]`. The scaling is applied as `(energies - scaling[1]) / scaling[0]`.
+
+    References:
+        [1] A. Ferreira, and E. R. Mucciolo, Phys. Rev. Lett. 115, 106601 (2015).
+    """
+
+    def __init__(
+        self,
+        moments: Array,
+        grid: Array,
+        scaling: tuple[float, float],
+        eta: float = 1e-2,
+        trace: bool = False,
+        include_real: bool = True,
+        max_cycle: int | None = None,
+    ):
+        """Initialise the solver.
+
+        Args:
+            moments: Chebyshev moments of the Green's function.
+            grid: Real frequency grid upon which to evaluate the Green's function.
+            scaling: Scaling factors to ensure the energy scale of the Lehmann representation is in
+                `[-1, 1]`. The scaling is applied as `(energies - scaling[1]) / scaling[0]`.
+            eta: The broadening parameter.
+            trace: Whether to return only the trace.
+            include_real: Whether to include the real part of the Green's function.
+            max_cycle: Maximum number of iterations.
+        """
+        self._moments = moments
+        self._grid = grid
+        self._scaling = scaling
+        self.eta = eta
+        self.trace = trace
+        self.include_real = include_real
+        self.max_cycle = max_cycle if max_cycle is not None else _infer_max_cycle(moments)
+
+    def kernel(self, iteration: int | None = None) -> Array:
+        """Run the solver.
+
+        Args:
+            iteration: The iteration number.
+
+        Returns:
+            The Green's function on the real frequency grid.
+        """
+        if iteration is None:
+            iteration = self.max_cycle
+
+        # Get the moments -- allow input to already be traced
+        moments = util.as_trace(self.moments[: iteration + 1], 3).astype(complex)
+
+        # Scale the grid
+        scaled_grid = (self.grid - self.scaling[1]) / self.scaling[0]
+        scaled_eta = self.eta / self.scaling[0]
+        shifted_grid = scaled_grid + 1j * scaled_eta
+
+        # Initialise factors
+        numerator = shifted_grid - 1j * np.sqrt(1 - shifted_grid**2)
+        denominator = np.sqrt(1 - shifted_grid**2)
+
+        # Iteratively compute the Green's function
+        shape = (self.grid.size,) if self.trace else (self.grid.size, self.nphys, self.nphys)
+        greens_function = np.zeros(shape, dtype=complex)
+        kernel = 1.0 / denominator
+        for cycle in range(iteration + 1):
+            factor = -1.0j * (2.0 - int(cycle == 0)) / (self.scaling[0] * np.pi)
+            greens_function -= einsum("z,...->z...", kernel, moments[cycle]) * factor
+            kernel *= numerator
+
+        # FIXME: Where have I lost this?
+        greens_function = -greens_function.conj()
+
+        return greens_function if self.include_real else greens_function.imag
+
+    @property
+    def moments(self) -> Array:
+        """Get the moments of the self-energy."""
+        return self._moments
+
+    @property
+    def grid(self) -> Array:
+        """Get the real frequency grid."""
+        return self._grid
+
+    @property
+    def scaling(self) -> tuple[float, float]:
+        """Get the scaling factors."""
+        return self._scaling
+
+    @property
+    def nphys(self) -> int:
+        """Get the number of physical degrees of freedom."""
+        return self.moments.shape[-1]
