15e2073ee8ca70e92f191dd950082b4bcf2a00be
Oliver Backhouse <olbackhouse@gmail.com>
2025-06-06 22:44:37 +0100

    Rich printing

---
+++ dyson/printing.py
@@ -0,0 +1,212 @@
+"""Printing utilities."""
+
+from __future__ import annotations
+
+import importlib
+import os
+import subprocess
+from typing import TYPE_CHECKING
+
+from rich.console import Console
+from rich.theme import Theme
+from rich.table import Table
+from rich import box
+
+from dyson import __version__
+
+if TYPE_CHECKING:
+    from typing import Any, Literal
+
+
+theme = Theme(
+    {
+        "good": "green",
+        "okay": "yellow",
+        "bad": "red",
+        "output": "cyan",
+        "input": "bright_magenta",
+        "method": "bold underline",
+        "header": "bold",
+    }
+)
+
+console = Console(
+    highlight=False,
+    theme=theme,
+    log_path=False,
+    quiet=os.environ.get("DYSON_QUIET", "").lower() in ("1", "true"),
+)
+
+HEADER = r"""     _
+    | |
+  __| | _   _  ___   ___   _ __
+ / _` || | | |/ __| / _ \ | '_ \
+| (_| || |_| |\__ \| (_) || | | |
+ \__,_| \__, ||___/ \___/ |_| |_|
+         __/ |
+        |___/  %s
+"""
+
+
+def init_console() -> None:
+    """Initialise the console with a header."""
+
+    if globals().get("_DYSON_LOG_INITIALISED", False):
+        return
+
+    # Print header
+    header_size = max([len(line) for line in HEADER.split("\n")])
+    header_with_version = "[header]" + HEADER + "[/header]"
+    header_with_version %= (" " * (18 - len(__version__)) + "[input]" + __version__ + "[/input]")
+    console.print(header_with_version)
+
+    # Print versions of dependencies and ebcc
+    def get_git_hash(directory):
+        git_directory = os.path.join(directory, ".git")
+        cmd = ["git", "--git-dir=%s" % git_directory, "rev-parse", "--short", "HEAD"]
+        try:
+            git_hash = subprocess.check_output(
+                cmd, universal_newlines=True, stderr=subprocess.STDOUT
+            ).rstrip()
+        except subprocess.CalledProcessError:
+            git_hash = "N/A"
+        return git_hash
+
+    for name in ["numpy", "pyscf", "dyson"]:
+        module = importlib.import_module(name)
+        git_hash = get_git_hash(os.path.join(os.path.dirname(module.__file__), ".."))
+        console.print(f"{name}:")
+        console.print(f" > Version:  [input]{module.__version__}[/]")
+        console.print(f" > Git hash: [input]{git_hash}[/]")
+
+    console.print("OMP_NUM_THREADS = [input]%s[/]" % os.environ.get("OMP_NUM_THREADS", ""))
+
+    globals()["_DYSON_LOG_INITIALISED"] = True
+
+
+class Quiet:
+    """Context manager to disable console output."""
+
+    def __init__(self, console: Console = console):
+        """Initialise the object."""
+        self._memo: list[bool] = []
+        self._console = console
+
+    def __enter__(self) -> None:
+        """Enter the context manager."""
+        self._memo.append(self.console.quiet)
+        self.console.quiet = True
+
+    def __exit__(self, *args: Any) -> None:
+        """Exit the context manager."""
+        quiet = self._memo.pop()
+        self.console.quiet = quiet
+
+    def __call__(self) -> None:
+        """Call the context manager."""
+        self.console.quiet = True
+
+    @property
+    def console(self) -> Console:
+        """Get the console."""
+        return self._console
+
+
+quiet = Quiet(console)
+
+
+def rate_error(
+    value: float | complex, threshold: float, threshold_okay: float | None = None
+) -> Literal["good", "okay", "bad"]:
+    """Rate the error based on a threshold.
+
+    Args:
+        value: The value to rate.
+        threshold: The threshold for the rating.
+        threshold_okay: Separate threshold for `"okay"` rating. Default is 10 times
+            :param:`threshold`.
+
+    Returns:
+        str: The rating, one of "good", "okay", or "bad".
+    """
+    if threshold_okay is None:
+        threshold_okay = 10 * threshold
+    if abs(value) < threshold:
+        return "good"
+    elif abs(value) < threshold_okay:
+        return "okay"
+    else:
+        return "bad"
+
+
+def format_float(
+    value: float | complex,
+    precision: int = 10,
+    scientific: bool = False,
+    threshold: float | None = None,
+) -> str:
+    """Format a float or complex number to a string with a given precision.
+
+    Args:
+        value: The value to format.
+        precision: The number of decimal places to include.
+        scientific: Whether to use scientific notation for large or small values.
+        threshold: If provided, the value will be rated based on this threshold.
+
+    Returns:
+        str: The formatted string.
+    """
+    if value.imag < (1e-1 ** precision):
+        value = value.real
+    out = f"{value:.{precision}g}" if scientific else f"{value:.{precision}f}"
+    if threshold is not None:
+        rating = rate_error(value, threshold)
+        out = f"[{rating}]{out}[/]"
+    return out
+
+
+class ConvergencePrinter:
+    """Table for printing convergence information."""
+
+    def __init__(
+        self,
+        quantities: tuple[str, ...],
+        quantity_errors: tuple[str, ...],
+        thresholds: tuple[float, ...],
+        console: Console = console,
+        cycle_name: str = "Cycle",
+    ):
+        """Initialise the object."""
+        self._console = console
+        self._table = Table(box=box.SIMPLE)
+        self._table.add_column(cycle_name, style="dim", justify="left")
+        for quantity in quantities:
+            self._table.add_column(quantity, justify="right")
+        for quantity_error in quantity_errors:
+            self._table.add_column(quantity_error, justify="right")
+        self._thresholds = thresholds
+
+    def add_row(
+        self,
+        cycle: int,
+        quantities: tuple[float, ...],
+        quantity_errors: tuple[float, ...],
+    ) -> None:
+        """Add a row to the table."""
+        self._table.add_row(
+            str(cycle),
+            *[format_float(quantity) for quantity in quantities],
+            *[
+                format_float(error, precision=4, scientific=True, threshold=threshold)
+                for error, threshold in zip(quantity_errors, self._thresholds)
+            ],
+        )
+
+    def print(self) -> None:
+        """Print the table."""
+        self._console.print(self._table)
+
+    @property
+    def thresholds(self) -> tuple[float, ...]:
+        """Get the thresholds."""
+        return self._thresholds
---
+++ dyson/util/misc.py
@@ -0,0 +1,27 @@
+"""Miscellaneous utility functions."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+import warnings
+
+from contextlib import contextmanager
+
+
+@contextmanager
+def catch_warnings(warning_type: type[Warning] = Warning) -> list[Warning]:
+    """Context manager to catch warnings.
+
+    Returns:
+        A list of caught warnings.
+    """
+    # Remove any user filters
+    user_filters = warnings.filters[:]
+    warnings.simplefilter("always", warning_type)
+
+    # Catch warnings
+    with warnings.catch_warnings(record=True) as caught_warnings:
+        yield caught_warnings
+
+    # Restore user filters
+    warnings.filters = user_filters
