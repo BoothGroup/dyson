c619b1f12a9c2ad1833b975d6b02b17e6a1407a7
Oliver Backhouse <olbackhouse@gmail.com>
2025-07-20 10:39:13 +0100

    Try removing indents in reST

--- dyson/__init__.py
+++ dyson/__init__.py
@@ -7,53 +7,53 @@ Dyson equation solvers in :mod:`dyson` are general solvers that accept a variety
 represent self-energies or existing Green's functions, and solve the Dyson equation in some fashion
 to obtain either
 
-    a) a static spectral representation that can be projected into a static Lehmann representation
-        of the Green's function or self-energy, or
-    b) a dynamic Green's function.
+a) a static spectral representation that can be projected into a static Lehmann representation
+   of the Green's function or self-energy, or
+b) a dynamic Green's function.
 
 The self-energy and Green's function are represented in the following ways:
 
-    +-------------------+--------------------------------------------------------------------------+
-    | Representation    | Description                                                              |
-    | :---------------- | :----------------------------------------------------------------------- |
-    | Spectral          | Eigenvalues and eigenvectors of the static self-energy supermatrix,
-                          from which the Lehmann representation of the self-energy or Green's
-                          function can be constructed.                                             |
-    | Lehmann           | The Lehmann representation of the self-energy or Green's function,
-                          consisting of pole energies and their couplings to a physical space.     |
-    | Dynamic           | The dynamic self-energy or Green's function, represented as a series of
-                          arrays at each point on a grid of time or frequency points.              |
-    +-------------------+--------------------------------------------------------------------------+
++-------------------+--------------------------------------------------------------------------+
+| Representation    | Description                                                              |
+| :---------------- | :----------------------------------------------------------------------- |
+| Spectral          | Eigenvalues and eigenvectors of the static self-energy supermatrix,
+                      from which the Lehmann representation of the self-energy or Green's
+                      function can be constructed.                                             |
+| Lehmann           | The Lehmann representation of the self-energy or Green's function,
+                      consisting of pole energies and their couplings to a physical space.     |
+| Dynamic           | The dynamic self-energy or Green's function, represented as a series of
+                      arrays at each point on a grid of time or frequency points.              |
++-------------------+--------------------------------------------------------------------------+
 
 The available static solvers are, along with their expected inputs:
 
-    +-------------------+--------------------------------------------------------------------------+
-    | Solver            | Inputs                                                                   |
-    | :---------------- | :----------------------------------------------------------------------- |
-    | Exact             | Supermatrix of the static and dynamic self-energy.                       |
-    | Davidson          | Matrix-vector operation and diagonal of the supermatrix of the static
-                          ad dynamic self-energy.                                                  |
-    | Downfolded        | Static self-energy and function returning the dynamic self-energy at a
-                          given frequency.                                                         |
-    | MBLSE             | Static self-energy and moments of the dynamic self-energy.               |
-    | MBLGF             | Moments of the dynamic Green's function.                                 |
-    | AufbauPrinciple   | Static self-energy, Lehmann representation of the dynamic self-energy,
-                          and the target number of electrons.                                      |
-    | AuxiliaryShift    | Static self-energy, Lehmann representation of the dynamic self-energy,
-                          and the target number of electrons.                                      |
-    | DensityRelaxation | Lehmann representation of the dynamic self-energy, function returning
-                          the Fock matrix at a given density, and the target number of electrons.  |
-    +-------------------+--------------------------------------------------------------------------+
++-------------------+--------------------------------------------------------------------------+
+| Solver            | Inputs                                                                   |
+| :---------------- | :----------------------------------------------------------------------- |
+| Exact             | Supermatrix of the static and dynamic self-energy.                       |
+| Davidson          | Matrix-vector operation and diagonal of the supermatrix of the static
+                      ad dynamic self-energy.                                                  |
+| Downfolded        | Static self-energy and function returning the dynamic self-energy at a
+                      given frequency.                                                         |
+| MBLSE             | Static self-energy and moments of the dynamic self-energy.               |
+| MBLGF             | Moments of the dynamic Green's function.                                 |
+| AufbauPrinciple   | Static self-energy, Lehmann representation of the dynamic self-energy,
+                      and the target number of electrons.                                      |
+| AuxiliaryShift    | Static self-energy, Lehmann representation of the dynamic self-energy,
+                      and the target number of electrons.                                      |
+| DensityRelaxation | Lehmann representation of the dynamic self-energy, function returning
+                      the Fock matrix at a given density, and the target number of electrons.  |
++-------------------+--------------------------------------------------------------------------+
 
 For dynamic solvers, all solvers require the grid parameters, along with:
 
-    +-------------------+--------------------------------------------------------------------------+
-    | Solver            | Inputs                                                                   |
-    | :---------------- | :----------------------------------------------------------------------- |
-    | CorrectionVector  | Matrix-vector operation and diagonal of the supermatrix of the static
-                          and dynamic self-energy.                                                 |
-    | CPGF              | Chebyshev polynomial moments of the dynamic Green's function.            |
-    +-------------------+--------------------------------------------------------------------------+
++-------------------+--------------------------------------------------------------------------+
+| Solver            | Inputs                                                                   |
+| :---------------- | :----------------------------------------------------------------------- |
+| CorrectionVector  | Matrix-vector operation and diagonal of the supermatrix of the static
+                      and dynamic self-energy.                                                 |
+| CPGF              | Chebyshev polynomial moments of the dynamic Green's function.            |
++-------------------+--------------------------------------------------------------------------+
 
 For a full accounting of the inputs and their types, please see the documentation for each solver.
 
@@ -61,22 +61,22 @@ A number of classes are provided to represent the expressions needed to construc
 different levels of theory. These expressions are all implemented for RHF references, with other
 spin symmetries left to the user to implement as needed. The available expressions are:
 
-    +-------------------+--------------------------------------------------------------------------+
-    | Expression        | Description                                                              |
-    | :---------------- | :----------------------------------------------------------------------- |
-    | HF                | Hartree--Fock (mean-field) ground state, exploiting Koopmans' theorem
-                          for the excited states.                                                  |
-    | CCSD              | Coupled cluster singles and doubles ground state, and the respective
-                          equation-of-motion method for the excited states.                        |
-    | FCI               | Full configuration interaction (exact diagonalisation) ground and
-                          excited states.                                                          |
-    | ADC2              | Algebraic diagrammatic construction second order excited states, based
-                          on a mean-field ground state.                                            |
-    | ADC2x             | Algebraic diagrammatic construction extended second order excited
-                          states, based on a mean-field ground state.                              |
-    | TDAGW             | GW theory with the Tamm--Dancoff approximation for the excited states,
-                          based on a mean-field ground state.                                      |
-    +-------------------+--------------------------------------------------------------------------+
++-------------------+--------------------------------------------------------------------------+
+| Expression        | Description                                                              |
+| :---------------- | :----------------------------------------------------------------------- |
+| HF                | Hartree--Fock (mean-field) ground state, exploiting Koopmans' theorem
+                      for the excited states.                                                  |
+| CCSD              | Coupled cluster singles and doubles ground state, and the respective
+                      equation-of-motion method for the excited states.                        |
+| FCI               | Full configuration interaction (exact diagonalisation) ground and
+                      excited states.                                                          |
+| ADC2              | Algebraic diagrammatic construction second order excited states, based
+                      on a mean-field ground state.                                            |
+| ADC2x             | Algebraic diagrammatic construction extended second order excited
+                      states, based on a mean-field ground state.                              |
+| TDAGW             | GW theory with the Tamm--Dancoff approximation for the excited states,
+                      based on a mean-field ground state.                                      |
++-------------------+--------------------------------------------------------------------------+
 
 
 Submodules
